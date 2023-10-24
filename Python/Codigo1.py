548eb4a9460d0a7b438c78af401b7d2cbfdee4a4
colival03 <146434716+colival03@users.noreply.github.com>
2023-10-24 20:01:16 +0200

    Realizados los ejercicios codigos 1,2,3 y los enunciados 1,2,3 el día 24/10/2023 a las 20:01

--- Python/Codigo1.py
+++ Python/Codigo1.py
@@ -22,3 +22,6 @@ while final != 0:
 
     print( "¿Quiere finalizar el proceso? Teclee 0 para salir.")
     final= int(input())
+
+'''Realiza un programa en el que pida al usuario tres números distintos y de los tres que muestre el mayor de los tres y
+   y para finalizar el programa presione la tecla 0.'''
